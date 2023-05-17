418651f11e3f4ace1f8d7b7c9ea7a5f2367f81f3
HazratAli <hazrat.arisaftech@gmail.com>
2023-05-17 09:37:31 +0600

    added env example file

--- test/blog.js
+++ test/blog.js
@@ -44,7 +44,7 @@ describe("BLOG API", () => {
   describe("POST /blogpost/:userid", () => {
     it("It should POST a blog", (done) => {
       const obj = {
-        title: "testing",
+        title: "testing final",
         context: "This is context",
       };
       const userId = "645a091da776c221d8336880";
@@ -54,10 +54,9 @@ describe("BLOG API", () => {
         .send(obj)
         .set(
           "Cookie",
-          "accessToken=	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWEwOTFkYTc3NmMyMjFkODMzNjg4MCIsImlhdCI6MTY4NDIyNjUzNX0.qqeMN_Fn-zVs3srmQGl2YmnU80rJADktQ-EiLQlfq5Y"
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWEwOTFkYTc3NmMyMjFkODMzNjg4MCIsImlhdCI6MTY4NDIyNjUzNX0.qqeMN_Fn-zVs3srmQGl2YmnU80rJADktQ-EiLQlfq5Y"
         )
         .end((err, res) => {
-          // should.not.exist(err);
           res.body.should.not.have(err);
           res.body.should.have.property("title");
           res.body.should.have.property("context");
